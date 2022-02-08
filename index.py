c08736f5f4b724695719bbe98886e477ef4cc80c
Ileen Fan <ifan2011@gmail.com>
2022-02-07 16:25:46 -0800

    created index and modify tokenizer

--- index.py
+++ index.py
@@ -1,23 +1,26 @@
 from collections import defaultdict, Counter
 from Posting import Posting
+from tokenizer import parse_text, tokenize
 import os
 import json
 
 inverted_index = defaultdict(list)
 directory = './DEV'
 
-for domain in os.scandir(directory):  # each subdir = web domain
-    if domain.is_dir():
-        for page in os.scandir(domain.path):  # each file within subdir = webpage
-            if page.is_file():
-                with open(page.path) as file:
-                    json_data = json.loads(file.read())
-                    content = json_data['content']
-                    parsed_content =  # bsoup to parse html into a string of tokens
-                    token_mapping = Counter(parsed_content)
+def build_index():
+    for domain in os.scandir(directory):  # each subdir = web domain
+        if domain.is_dir():
+            for page in os.scandir(domain.path):  # each file within subdir = webpage
+                if page.is_file():
+                    with open(page.path) as file:
+                        json_data = json.loads(file.read())
+                        content = json_data['content']
+                        parsed_content = parse_text(content) # bsoup to parse html into a string of tokens
+                        token_mapping = Counter(tokenize(parsed_content))
+                        total_tokens = sum(token_mapping.values())
 
-                    for token, count in token_mapping.items():
-                        inverted_index[token].append(Posting(json_data['url'], count, sum(token_mapping.values())))
+                        for token, count in token_mapping.items():
+                            inverted_index[token].append(Posting(json_data['url'], count, total_tokens))
 
 def write_index_to_file(inverted_index):
     file = open('index.txt', 'w')
--- tokenizer.py
+++ tokenizer.py
@@ -5,11 +5,11 @@ from bs4 import BeautifulSoup
 # we don't want to exclude stopwords yet
 #stop_words = {'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves'}
 
-def pull_text(html: str) -> [str]:
+def parse_text(html: str) -> [str]:
     ''' given an html string, pulls text from the following tags:
         <p>, <h1/2/3/4/5/6>, <strong>, <i>, <b>, <u>, <title>, <meta name="author"/"description"/"keywords">
     '''
-    res = []
+    parsed_str = ' '
     tags = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "strong", "i", "b", "u", "title"]
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -18,19 +18,17 @@ def pull_text(html: str) -> [str]:
 
     for t in tags_text:
         if t.text.strip() != '':
-            res.append(t.text.strip())
+            parsed_str += t.text + ' '
 
     for m in meta_text:
         if m.has_attr("content") and m["content"].strip() != '':
-            res.append(m["content"].strip())
+            parsed_str += m["content"] + ' '
 
-    return res
-
-# we don't want stopwords yet
-# stop_words = {'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves'}
+    return parsed_str
 
 def tokenize(string) -> [str]:
-    ''' given a string, returns list of tokens
+    '''
+    given a string, returns list of tokens
     '''
     tokens = []
     try:
