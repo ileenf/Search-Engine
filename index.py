77c2eb870f70ff29a43dc7a2e17a42b8928665fd
Kim Nguyen <shridharsona@gmail.com>
2022-02-16 14:16:23 -0800

    added id_url_map functions

--- index.py
+++ index.py
@@ -3,18 +3,31 @@ from Posting import Posting
 from tokenizer import parse_text, tokenize
 import os
 import json
+import psutil
 
 #directory = './DEV'
 
-def build_index(base_dir):
-    cur_docID = 0
+def build_partial_index(base_dir: str, batch_sz: int)->dict:
+    pass
 
+def build_index(base_dir: str, batch_sz: int)->dict:
+    pindex_num = 1
+    cur_docID = 0
+    #doc_id_url_map = dict()
     inverted_index = defaultdict(list)
-    for domain in os.scandir(base_dir):  # each subdir = web domain
+
+    # retrieve batch of corpus (50 docs?)
+    # create posting list
+    # if mem < 50%, retrieve another batch 
+    # else, dump
+    # clear dict
+
+###################################
+
+    for domain in os.scandir(base_dir):             # each subdir = web domain
         if domain.is_dir():
-            for page in os.scandir(domain.path):  # each file within subdir = webpage
+            for page in os.scandir(domain.path):    # each file within subdir = webpage
                 if page.is_file():
-                    
                     with open(page.path) as file:
                         cur_docID += 1
                         json_data = json.loads(file.read())
@@ -23,18 +36,24 @@ def build_index(base_dir):
 
                         token_mapping = Counter(tokenize(parsed_content))
                         total_tokens = sum(token_mapping.values())
-
                         for token, count in token_mapping.items(): 
                             inverted_index[token].append(Posting(cur_docID, count, total_tokens))
     return inverted_index
 
-def write_index_to_file(inverted_index):
-    file = open('index.txt', 'w')
-    for token in sorted(inverted_index):            # sort by keys
-        file.write(token + ": ")
+
+
+def write_index_to_file(inverted_index: dict):
+    file = open(f'index.txt', 'w')
+    for token in sorted(inverted_index):                            # sort by keys
+        file.write(f'{token}, {len(inverted_index[token])}: ')      # token, termdocfreq: 
         for posting in inverted_index[token]:
             posting_json = json.dumps(posting.__dict__)
             file.write(posting_json)
         file.write('\n')
 
     file.close()
+
+def write_id_url_map(id_url_map:dict):
+    with open('id_url_map.txt', 'w') as file:
+        for id, url in id_url_map.items():
+            file.write(f'{id}: {url}\n')
